45a2e1c3c0edb60c4dba639f236566e279868717
drelocatelli <andressalocatelli@hotmail.com>
2023-04-28 13:54:42 -0300

    setting position of element

--- src/components/panels/right/properties/forms/position.vue
+++ src/components/panels/right/properties/forms/position.vue
@@ -2,23 +2,23 @@
 import { computed } from 'vue';
 import useFocusStore from '../../../../../store/focus';
 
-const props = defineProps({ position: {required: true, type: String} });
-
+const props = defineProps({ position: { required: true, type: String } });
 const focusStore = useFocusStore();
+const shape = focusStore.action!.shape;
 const shapePosition = computed(() => {
     //@ts-ignore
-    return focusStore.action!.shape!.getPosition()[props.position]
+    return focusStore.action!.shape!.getPosition()[props.position];
 });
 
-const setPosition = () => {
-    
+const setPosition = (e: Event) => {
+    const target = e.target as HTMLInputElement;
+    shape?.setAttr(props.position, target.value);
 };
-
 </script>
 
 <template>
     <div class="item" :title="`Posição eixo ${props.position}`">
-        <label :for="`position-${props.position}`" style="font-size: 13px; opacity: .6;"> {{ props.position.toUpperCase() }} </label>
-        <input :id="`position-${props.position}`" :value="shapePosition" type="number">
+        <label :for="`position-${props.position}`" style="font-size: 13px; opacity: 0.6"> {{ props.position.toUpperCase() }} </label>
+        <input :id="`position-${props.position}`" :value="shapePosition" type="number" :oninput="setPosition" />
     </div>
 </template>
