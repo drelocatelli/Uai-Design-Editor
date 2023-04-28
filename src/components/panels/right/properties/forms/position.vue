e84870673f26ea3120e37488fc36e084dff9409c
drelocatelli <andressalocatelli@hotmail.com>
2023-04-27 22:11:42 -0300

    init setting position

---
+++ src/components/panels/right/properties/forms/position.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup>
+import { Shape } from 'konva/lib/Shape';
+import { computed } from 'vue';
+
+const props = defineProps({ shape: { required: true, type: Shape }, position: {required: true, type: String} });
+const position = computed(() => props.shape.getPosition());
+
+const setPosition = () => {};
+</script>
+
+<template>
+    <div class="item" :title="`Posição eixo ${props.position}`">
+        <label :for="`position-${props.position}`" style="font-size: 13px; opacity: .6;"> {{ props.position.toUpperCase() }} </label>
+        <input :id="`position-${props.position}`" :value="position[(props.position) as any]" type="number">
+    </div>
+</template>
--- src/components/panels/right/properties/shape.vue
+++ src/components/panels/right/properties/shape.vue
@@ -4,6 +4,7 @@ import useFocusStore from '../../../../store/focus';
 import FillPicker from '../input/fill.vue';
 import RoundedCorners from './forms/rounded-corners.vue';
 import GridPropertiesComponent from './forms/grid-properties.vue';
+import Position from './forms/position.vue';
 import { Shape } from 'konva/lib/Shape';
 
 const focusStore = useFocusStore();
@@ -17,6 +18,8 @@ const isRect = computed(() => shape.value.className == 'Rect');
         <div class="label" v-if="isRect">
             <h5>Propriedades da forma</h5>
             <GridPropertiesComponent x="3" y="2">
+                <Position :shape="(shape as Shape)" position="x" />
+                <Position :shape="(shape as Shape)" position="y" />
                 <RoundedCorners :shape="(shape as Shape)" />
             </GridPropertiesComponent>
         </div>
